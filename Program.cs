38888f283b8a9cd1f7373de035bc24cab1e1d20b
xili1342 <lucaliu217@outlook.com>
2022-10-16 12:14:59 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+
+      //this is a comment
+      Console.WriteLine(" .-.");   
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      //daning man
+      Console.WriteLine("c_     ___");
+      Console.WriteLine("　 \\__|__|__");
+      
+     }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+## Console Creatures
+
+this is the link for the project:
+
+ https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/build-web-apps-asp-net-beginner-c-sharp/modules/csharp-hello-world/projects/csharp-console-creatures
+
